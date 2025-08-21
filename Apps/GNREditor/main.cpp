a2a4dd9ad6619880213c786585145850aafabb37
gukazma <18017158729@163.com>
2025-08-21 12:11:12 +0800

    ✨ feat: 使用spdlog输出日志

--- Apps/GNREditor/main.cpp
+++ Apps/GNREditor/main.cpp
@@ -1,4 +1,7 @@
 ﻿#include <NRIFramework.h>
+#include <spdlog/spdlog.h>
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
+
 static const char *vendors[] = {
     "unknown",
     "NVIDIA",
@@ -26,21 +29,21 @@ public:
         // Initialize NRI and other components
         std::vector<nri::AdapterDesc> adapterDescs(adaptersNum);
         NRI_ABORT_ON_FAILURE(nri::nriEnumerateAdapters(adapterDescs.data(), adaptersNum)); // 传 nullptr 查询数量
-        printf("nriEnumerateAdapters: %u adapters reported\n", adaptersNum);
+        SPDLOG_INFO("nriEnumerateAdapters: {} adapters reported", adaptersNum);
         for (uint32_t i = 0; i < adaptersNum; i++)
         {
             const nri::AdapterDesc adapterDesc = adapterDescs[i];
 
-            printf("\nAdapter #%u\n", i + 1);
-            printf("\tName                 : %s\n", adapterDesc.name);
-            printf("\tVendor               : %s\n", vendors[int(adapterDesc.vendor)]);
-            printf("\tArchitecture         : %s\n", architectures[int(adapterDesc.architecture)]);
-            printf("\tVideo memory         : %" PRIu64 " Mb\n", adapterDesc.videoMemorySize >> 20);
-            printf("\tShared system memory : %" PRIu64 " Mb\n", adapterDesc.sharedSystemMemorySize >> 20);
-            printf("\tQueues               : {%u, %u, %u}\n", adapterDesc.queueNum[0], adapterDesc.queueNum[1],
-                   adapterDesc.queueNum[2]);
-            printf("\tID                   : 0x%08X\n", adapterDesc.deviceId);
-            printf("\tLUID                 : 0x%016" PRIX64 "\n", adapterDesc.luid);
+            SPDLOG_INFO("Adapter #{}:", i + 1);
+            SPDLOG_INFO("\tName                 : {}", adapterDesc.name);
+            SPDLOG_INFO("\tVendor               : {}", vendors[int(adapterDesc.vendor)]);
+            SPDLOG_INFO("\tArchitecture         : {}", architectures[int(adapterDesc.architecture)]);
+            SPDLOG_INFO("\tVideo memory         : {} Mb", adapterDesc.videoMemorySize >> 20);
+            SPDLOG_INFO("\tShared system memory : {} Mb", adapterDesc.sharedSystemMemorySize >> 20);
+            SPDLOG_INFO("\tQueues               : {{ {}, {}, {} }}", adapterDesc.queueNum[0], adapterDesc.queueNum[1],
+                        adapterDesc.queueNum[2]);
+            SPDLOG_INFO("\tID                   : 0x{:08X}", adapterDesc.deviceId);
+            SPDLOG_INFO("\tLUID                 : 0x{:016X}", adapterDesc.luid);
         }
 
         return true;
